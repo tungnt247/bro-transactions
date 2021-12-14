89d0723b8e83fa3a11ff4033b2c10b53499157f5
hungpq96 <quanghwng22@gmail.com>
2021-12-14 13:54:18 +0700

    Open authentication urls

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
     path('profile/', view=views.profile),
 ]
